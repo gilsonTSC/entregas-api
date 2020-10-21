c19fd35fc8ed2452b2ece5e7c6d16dfa2785ec5e
Gilson <gilson.costa@dce.ufpb.br>
2020-10-20 21:47:54 -0300

    Implementando os repositores

---
+++ src/main/java/com/gilsontsc/entregas/api/repository/CoordenadaRepository.java
@@ -0,0 +1,9 @@
+package com.gilsontsc.entregas.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gilsontsc.entregas.api.entity.CoordenadaEntity;
+
+public interface CoordenadaRepository extends JpaRepository<CoordenadaEntity, Long> {
+
+}
---
+++ src/main/java/com/gilsontsc/entregas/api/repository/RotaRepository.java
@@ -0,0 +1,9 @@
+package com.gilsontsc.entregas.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gilsontsc.entregas.api.entity.RotaEntity;
+
+public interface RotaRepository extends JpaRepository<RotaEntity, Long> {
+
+}
